7e3175e678bf4d6dd4aff95a95910c6da18d1256
Anshul <anshul.iitdhn@gmail.com>
2022-04-10 21:03:04 +0530

    circularly linked list implementation

--- Linked list/circularly_linked_list.py
+++ Linked list/circularly_linked_list.py
@@ -1,35 +1,77 @@
 class Node:
-    """ A singly-linked node. """
+    """ A singly-linked list node. """
 
     def __init__(self, data=None):
         self.data = data
         self.next = None
-    
-    def _str_ (self):
-        return str(self.data)
 
 
 class CircularlyLinkedList:
     def __init__(self):
+        self.head = None
         self.tail = None
         self.count = 0
 
     def append(self, data):
-        """Append a new data node to singly linked list """
-        pass
+        """Append a new data node to circularly linked list """
+        # create a new node
+        new_node = Node(data)
+
+        # check if list is empty, if yes then update the head
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.tail.next = self.head
+        self.count += 1
 
     def iter(self):
         """Iterate through the list"""
-        pass
+        current = self.head
+        counter = 0
+        while current:
+            if counter == self.count:
+                break
+            val = current.data
+            current = current.next
+            counter += 1
+            yield val
 
-    def traverse(self):
+    def print(self):
         """traverse and print the linked list"""
-        pass
+        print("----------------CLL---------------")
+        for i in self.iter():
+            print(i)
 
     def delete(self, data):
-        "delete an element from the linked list"
-        pass
+        "delete an element from the circularly linked list"
+        prev = self.head
+        current = self.head
+        while current == prev or prev != self.tail:
+            if current.data == data:
+                if current == self.head:
+                    self.head = current.next
+                    self.tail.next = self.head
+                else:
+                    prev.next = current.next
+                self.count -= 1
+                break
+            prev = current
+            current = current.next
 
-    def search(self,data):
+    def contain(self, data):
         """search an element in the linked list"""
-        pass
+        curr = self.head
+        for node in self.iter():
+            if node == data:
+                return True
+            curr = curr.next
+        return False
+
+    def clear(self):
+        """clear the entire list"""
+        self.head = None
+        self.tail = None
+        self.count = 0
--- Linked list/main.py
+++ Linked list/main.py
@@ -1,5 +1,6 @@
 from singly_linked_list import SinglyLinkedList
 from doubly_linked_list import DoublyLinkedList
+from circularly_linked_list import CircularlyLinkedList
 
 
 def print_LL(LL):
@@ -12,15 +13,16 @@ def print_LL(LL):
 def main():
 
     # books = SinglyLinkedList()
-    books = DoublyLinkedList()
+    # books = DoublyLinkedList()
+    books = CircularlyLinkedList()
     books.append("book 1")
     books.append("book 2")
     books.append("book 3")
     books.append("book 4")
     books.print()
-    books.delete("book 3")
+    books.delete("book 1")
     books.print()
-    print(books.contain("book 4"))
+    print(books.contain("book 1"))
 
 
 if __name__ == "__main__":
